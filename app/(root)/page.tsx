8f14cb79be53bcfd7e2391384404194314840a78
NS750 <130935915+NS750@users.noreply.github.com>
2024-08-18 20:47:45 +0200

    Implement auth, appwrite and sentry

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
 import HeaderBox from '@/components/ui/HeaderBox'
 import RightSidebar from '@/components/ui/RightSidebar';
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
 
-const Home = () => {
-
-const loggedIn = {firstName: 'Enes', lastName: 'Gjurgjiali' ,email: 'enesgjurgjiali04@gmail.com'};
+const Home = async () => {
+const loggedIn = await getLoggedInUser();
 
   return (
     <section className='home'>
@@ -14,7 +14,7 @@ const loggedIn = {firstName: 'Enes', lastName: 'Gjurgjiali' ,email: 'enesgjurgji
           <HeaderBox 
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.name || 'Guest'}
             subtext='Access and manage your account and transactions efficiently'
           />
 
--- components/ui/RightSidebar.tsx
+++ components/ui/RightSidebar.tsx
@@ -2,19 +2,20 @@ import Image from "next/image"
 import Link from "next/link"
 import BankCard from "./BankCard"
 
-const RightSidebar = ({ user, transactions, banks}: RightSidebarProps) => {
+
+const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   return (
     <aside className='right-sidebar'>
       <section className='flex flex-col pb-8'>
         <div className="profile-banner" />
           <div className="profile">
             <div className="profile-img">
-              <span className='text-5xl font-bold text-blue-500'>{user.firstName[0]}</span>
+              <span className='text-5xl font-bold text-blue-500'>{user.name[0]}</span>
             </div>
 
             <div className="profile-details">
               <h1 className='profile-name'>
-                {user.firstName} {user.lastName}
+                {user.name}
               </h1>
               <p className='profile-email'>
                 {user.email}
@@ -46,7 +47,7 @@ const RightSidebar = ({ user, transactions, banks}: RightSidebarProps) => {
               <BankCard 
                 key={banks[0].$id}
                 account={banks[0]}
-                userName={`${user.firstName} ${user.lastName} `} showBalance={false}
+                userName={user.name} showBalance={false}
               />
             </div>
             {banks[1] && (
@@ -54,7 +55,7 @@ const RightSidebar = ({ user, transactions, banks}: RightSidebarProps) => {
                 <BankCard 
                  key={banks[1].$id}
                  account={banks[1]}
-                 userName={`${user.firstName} ${user.lastName} `} showBalance={false}
+                 userName={user.name} showBalance={false}
                 />
               </div>
             )}
